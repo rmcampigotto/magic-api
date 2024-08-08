5de92e2e14552c6737b5c3080fc1a2ff40566805
rmcampigotto <rmcampigotto@gmail.com>
2024-08-08 20:35:08 -0300

    schema-0808

---
+++ src/commander-deck/commander.schema.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema, model } from "mongoose";
+const AutoIncrement = require('mongoose-sequence');
+
+const commanderSchema = new Schema({
+    commanderID: Number,
+    name: { type: String, required: true },
+    color: { type: String, required: true },
+    cards: { type: Array, requires: true }
+}, {
+    timestamps: true
+});
+
+commanderSchema.plugin(AutoIncrement(mongoose), { inc_field: 'commanderID' });
+
+export default model('Commander', commanderSchema);

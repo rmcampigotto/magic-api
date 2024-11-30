ec31b1a34c5c9c4bc90f22af1988a2f7e671a760
Hudson-Matias <hudson.um@gmail.com>
2024-11-30 12:37:47 -0300

    configurando as notificações

---
+++ src/notification/notification.gateway.ts
@@ -0,0 +1,24 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway()
+export class NotificationGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server;
+
+  handleConnection(client: any) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: any) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
+  notifyAll(event: string, message: any) {
+    this.server.emit(event, message);
+  }
+}
